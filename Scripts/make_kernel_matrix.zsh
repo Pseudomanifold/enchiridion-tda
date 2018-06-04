a76d8e4910fd17ebbaef697605b979a31791281e
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-06-04 18:10:11 +0200

    Using file list generation to create kernel matrix
    Else, you *will* get errors of the form "argument list too long",
    depending on how your system is being configured.

--- Scripts/make_kernel_matrix.zsh
+++ Scripts/make_kernel_matrix.zsh
@@ -4,5 +4,6 @@ BIN=topological_distance
 INPUT_PATH="../Results"
 
 for DIR in ${INPUT_PATH}/*; do
-  $BIN -n -i -p 1 -k $DIR/*_d*.txt > "$DIR/Kernel.txt"
+  find $DIR -name "*_*.txt" -print > "$DIR/Files.txt"
+  $BIN -n -i -p 1 -k -l "$DIR/Files.txt" > "$DIR/Kernel.txt"
 done
