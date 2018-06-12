28d0336bf74a29c2bf8407c9ca7f8e11f10cca1f
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-06-12 17:37:29 +0200

    Added compression for calculation script

--- Scripts/make_kernel_matrix.zsh
+++ Scripts/make_kernel_matrix.zsh
@@ -6,4 +6,6 @@ INPUT_PATH="../Results"
 for DIR in ${INPUT_PATH}/*; do
   find -s $DIR -name "*_*.txt" -print > "$DIR/Files.txt"
   $BIN -n -i -p 1 -k -l "$DIR/Files.txt" > "$DIR/Kernel.txt"
+  gzip --best "$DIR/Kernel.txt"
+  gzip --best "$DIR/Labels.txt"
 done
